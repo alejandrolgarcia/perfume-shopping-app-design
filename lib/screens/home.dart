bd7a130c29466288fea13693337f9b8ada664482
Luis Garcia <lalejandro.1988@gmail.com>
2020-10-01 22:27:17 -0600

    search - see all

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -34,14 +34,22 @@ class Home extends StatelessWidget {
       ),
 
       body: Container(
-        
+
+        child: Column(
+          children: [
+
+            search(),
+            count()
+
+          ],
+        ),
 
       ),
 
     );
   }
 
-  Container myCart() {
+  Widget myCart() {
     return Container(
       width: 110,
       margin: EdgeInsets.only(
@@ -64,6 +72,78 @@ class Home extends StatelessWidget {
           Text(
             'My Cart',
             style: TextStyle(color: redColor),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget search() {
+    return Container(
+      decoration: BoxDecoration(),
+      padding: EdgeInsets.all(20),
+      child: TextField(
+        style: TextStyle(fontSize: 20),
+        obscureText: true,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.only(top: 20, bottom: 20),
+          filled: true,
+          fillColor: greyColor,
+          border: InputBorder.none,
+          prefixIcon: Icon(
+            Icons.search,
+            color: redColor,
+          ),
+          hintText: 'Search Perfume',
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: greyColor),
+            borderRadius: BorderRadius.circular(15)
+          ),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: greyColor),
+            borderRadius: BorderRadius.circular(15)
+          )
+        ),
+      ),
+    );
+  }
+
+  Widget count() {
+    return Container(
+      padding: EdgeInsets.only(left:20, top: 10, bottom: 10),
+      child: Row(
+        children: [
+          Text(
+            '2250',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          SizedBox(width: 15),
+          Container(
+            height: 35,
+            width: 110,
+            decoration: BoxDecoration(
+              color: redColor,
+              borderRadius: BorderRadius.circular(20)
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'SEE ALL',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16
+                  ),
+                ),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.white,
+                )
+              ],
+            ),
           )
         ],
       ),
